0f3d02dac90ed8715761702bc55ba275e140afff
ZazaDev <everaldoallan@gmail.com>
2025-03-09 19:32:07 -0300

    postgame is almost finished

--- Minesweeper.ex
+++ Minesweeper.ex
@@ -14,27 +14,21 @@ defmodule Minesweeper do
   def update_arr([h|t],n,v), do: [h|update_arr(t, n-1, v)]
 
   # get_pos/3 (get position): recebe um tabuleiro (matriz), uma linha (l) e uma coluna (c) (não precisa validar).
-  # Devolve o elemento na posicao tabuleiro[l,c]. Usar get_arr/2 na implementação
+  # Devolve o elemento na posicao tabuleiro[l,c].
   def get_pos(tab,l,c), do: get_arr(get_arr(tab, l), c)
 
   # update_pos/4 (update position): recebe um tabuleiro, uma linha, uma coluna e um novo valor. Devolve
-  # o tabuleiro modificado com o novo valor na posiçao linha x coluna. Usar update_arr/3 e get_arr/2 na implementação
-  def update_pos(tab,l,c,v) do
-    row = get_arr(tab, l)
-    newRow = update_arr(row, c, v)
-    update_arr(tab, l, newRow)
-  end
+  # o tabuleiro modificado com o novo valor na posiçao linha x coluna.
+  def update_pos(tab,l,c,v), do: update_arr(tab, l, update_arr(get_arr(tab, l), c, v))
 
   #-- is_mine/3: recebe um tabuleiro com o mapeamento das minas, uma linha, uma coluna. Devolve true caso a posição contenha
   # uma mina e false caso contrário.
   def is_mine(minas,l,c), do: get_pos(minas, l, c) #???
 
-  # is_valid_pos/3 recebe o tamanho do tabuleiro (ex, em um tabuleiro 9x9, o tamanho é 9),
-  # uma linha e uma coluna, e diz se essa posição é válida no tabuleiro.
+  # is_valid_pos/3 recebe o tamanho do tabuleiro, uma linha e uma coluna, e diz se essa posição é válida no tabuleiro.
   def is_valid_pos(size,l,c), do: l < size && c < size && l >= 0 && c >= 0
 
-  # valid_moves/3: Dado o tamanho do tabuleiro e uma posição atual (linha e coluna), retorna uma lista
-  # com todas as posições adjacentes à posição atual
+  # valid_moves/3: Dado o tamanho do tabuleiro e uma posição atual, retorna uma lista com todas as posições adjacentes à posição atual
   def valid_moves(tam,l,c) do
     directions = [
       {-1, -1}, {-1, 0},  {-1, 1},
@@ -47,18 +41,18 @@ defmodule Minesweeper do
     |> Enum.filter(fn {l, c} -> is_valid_pos(tam, l, c) end)
   end
 
-  # conta_minas_adj/3: recebe um tabuleiro com o mapeamento das minas e uma posicao (linha e coluna), e conta quantas minas
-  # existem nas posições adjacentes
+  # conta_minas_adj/3: recebe um tabuleiro com o mapeamento das minas e uma posicao, e conta quantas minas existem nas posições adjacentes
   def conta_minas_adj(minas,l,c) do
     valid_moves(Enum.count(minas),l,c)
     |> Enum.filter(fn {l, c} -> is_mine(minas,l,c) end)
     |> Enum.count()
   end
 
-  # abre_jogada/4: Função principal do jogo! Recebe uma posição a ser aberta (linha e coluna), o mapa de minas e o tabuleiro do jogo.
+  # abre_jogada/4: Função principal do jogo! Recebe uma posição a ser aberta, o mapa de minas e o tabuleiro do jogo.
   # Devolve como resposta o tabuleiro do jogo modificado com essa jogada.
   # Essa função é recursiva, pois no caso da entrada ser uma posição sem minas adjacentes, o algoritmo deve
   # seguir abrindo todas as posições adjacentes até que se encontre posições adjacentes à minas.
+  #
   # - Se a posição a ser aberta é uma mina, o tabuleiro não é modificado e encerra;
   # - Se a posição a ser aberta já foi aberta, o tabuleiro não é modificado e encerra;
   # - Se a posição a ser aberta é adjacente a uma ou mais minas, devolver o tabuleiro modificado com o número de minas adjacentes na posição aberta;
@@ -66,7 +60,7 @@ defmodule Minesweeper do
   def abre_jogada(l, c, minas, tab) do
     adjMines = conta_minas_adj(minas,l,c)
     cond do
-      is_mine(minas,l,c) -> end_game(minas, tab)        #TODO: mine, end game? it throws an error after
+      is_mine(minas,l,c) -> tab                         #mine, end game? it throws an error after
       get_pos(tab,l,c) != "-" -> tab                    #already open, return tab
       adjMines > 0 -> update_pos(tab,l,c,adjMines)      #adjMines, return tab with num
       true ->                                           #open with 0 and recursively open other adjacent positions
@@ -76,8 +70,7 @@ defmodule Minesweeper do
     end
   end
 
-# abre_posicao/4, que recebe um tabueiro de jogos, o mapa de minas, uma linha e uma coluna
-# Essa função verifica:
+# abre_posicao/4: recebe um tabueiro de jogos, o mapa de minas, uma linha e uma coluna e verifica:
 # - Se a posição {l,c} já está aberta (contém um número), então essa posição não deve ser modificada
 # - Se a posição {l,c} contém uma mina no mapa de minas, então marcar  com "*" no tabuleiro
 # - Se a posição {l,c} está fechada (contém "-"), escrever o número de minas adjascentes a esssa posição no tabuleiro (usar conta_minas)
@@ -85,7 +78,7 @@ defmodule Minesweeper do
     cond do
       is_mine(minas, l, c) -> update_pos(tab, l, c, "*")
       get_pos(tab, l, c) == "-" -> update_pos(tab, l, c, conta_minas_adj(tab, l, c))
-      true -> tab #TODO: idk if return tab here is correct
+      true -> tab
     end
   end
 
@@ -94,12 +87,15 @@ defmodule Minesweeper do
 # onde estão as minas e os números nas posições adjecentes às minas.Essa função é usada para mostrar
 # todo o tabuleiro no caso de vitória ou derrota. Para implementar esta função, usar a função abre_posicao/4
 
-#TODO: make ts work in conjunction with the engine code
+#TODO: fix mine counting after death
   def abre_tabuleiro(minas,tab) do
-    Enum.reduce(tab, fn {row, col} -> abre_posicao(tab, minas, row, col) end)
+    range = 0..(Enum.count(tab) - 1)
+    moves = for x <- range, y <- range, do: {x, y}
+
+    Enum.reduce(moves, tab, fn {row, col}, accTab -> abre_posicao(accTab, minas, row, col) end)
   end
 
-# board_to_string/1: -- Recebe o tabuleiro do jogo e devolve uma string que é a representação visual desse tabuleiro.
+# board_to_string/1: Recebe o tabuleiro do jogo e devolve uma string que é a representação visual desse tabuleiro.
 # Essa função é aplicada no tabuleiro antes de fazer o print dele na tela.
   def board_to_string(tab) do
     header = print_header(length(hd(tab)))
@@ -124,25 +120,25 @@ defmodule Minesweeper do
     "#{index}  " <> row_string <> " \n"
   end
 
-# gera_lista/2: recebe um inteiro n, um valor v, e gera uma lista contendo n vezes o valor v
+# gera_lista/2: recebe um inteiro n, um valor v, e gera uma lista contendo n vezes o valor v.
   def gera_lista(0,_v), do: []
   def gera_lista(n,v), do: [v|gera_lista(n-1, v)]
 
-# -- gera_tabuleiro/1: recebe o tamanho do tabuleiro de jogo e gera um tabuleiro  novo, todo fechado (todas as posições contém "-"). Usar gera_lista
+# -- gera_tabuleiro/1: recebe o tamanho do tabuleiro de jogo e gera um tabuleiro  novo, todo fechado (todas as posições contém "-").
   def gera_tabuleiro(n), do: gera_lista(n, gera_lista(n, "-"))
 
-# -- gera_mapa_de_minas/1: recebe o tamanho do tabuleiro e gera um mapa de minas zero, onde todas as posições contém false
+# -- gera_mapa_de_minas/1: recebe o tamanho do tabuleiro e gera um mapa de minas zero, onde todas as posições contém false.
   def gera_mapa_de_minas(n), do: gera_lista(n, gera_lista(n, false))
 
 
-# conta_fechadas/1: recebe um tabueleiro de jogo e conta quantas posições fechadas existem no tabuleiro (posições com "-")
+# conta_fechadas/1: recebe um tabuleiro de jogo e conta quantas posições fechadas existem no tabuleiro (posições com "-").
   def conta_fechadas(tab) do
     tab
     |> Enum.flat_map(& &1)
     |> Enum.count(&(&1 == "-"))
   end
 
-# -- conta_minas/1: Recebe o tabuleiro de Minas (MBoard) e conta quantas minas existem no jogo
+# -- conta_minas/1: recebe o tabuleiro de minas e conta quantas minas existem no jogo.
   def conta_minas(minas) do
     minas
     |> Enum.flat_map(& &1)
@@ -150,7 +146,7 @@ defmodule Minesweeper do
   end
 
 # end_game?/2: recebe o tabuleiro de minas, o tauleiro do jogo, e diz se o jogo acabou.
-# O jogo acabou quando o número de casas fechadas é igual ao numero de minas
+# O jogo acaba quando o número de casas fechadas é igual ao numero de minas.
   def end_game(minas,tab), do: conta_fechadas(tab) == conta_minas(minas)
 
 end
